2eea5e1d5d584cbda79d63bfd6c9f1be69fbd718
owenmc460 <owen.mcnally@ericsson.com>
2017-06-12 14:33:46 +0100

    First push of the Application
    First push:
      * Includes start of maps

---
+++ app/src/main/java/com/example/eowemcn/myapplication/MainActivity.java
@@ -0,0 +1,216 @@
+package com.example.eowemcn.myapplication;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Zone 1");
+        listDataHeader.add("Zone 3");
+        listDataHeader.add("Zone 4");
+        listDataHeader.add("Zone 5");
+        listDataHeader.add("Zone 6");
+        listDataHeader.add("Zone 7");
+        listDataHeader.add("Zone 8");
+        listDataHeader.add("Zone 9");
+
+
+        // Adding child data
+        List<String> zone1 = new ArrayList<String>();
+        zone1.add("Lars Magnus");
+        zone1.add("Reykjavik");
+        zone1.add("Nuuk");
+        zone1.add("Quiet Room 1-16");
+        zone1.add("Quiet Room 1-17");
+        zone1.add("Quiet Room 1-20");
+        zone1.add("Quiet Room 1-21");
+        zone1.add("Quiet Room 1-28");
+        zone1.add("Quiet Room 1-29");
+
+
+        List<String> zone3 = new ArrayList<String>();
+        zone3.add("Paris");
+        zone3.add("Athlone- Conference Room");
+        zone3.add("Quiet Room 3-36");
+        zone3.add("Quiet Room 3-44");
+        zone3.add("Quiet Room 3-45");
+
+
+        List<String> zone4 = new  ArrayList<String>();
+        zone4.add("Demo Room");
+        zone4.add("Kuala Lumpur");
+        zone4.add("Quiet Room 4-01");
+        zone4.add("Quiet Room 4-02");
+        zone4.add("Quiet Room 4-27");
+        zone4.add("Quiet Room 4-45");
+        zone4.add("Quiet Room 4-55");
+        zone4.add("Quiet Room 4-56");
+        zone4.add("Quiet Room 4-57");
+
+        List<String> zone5 = new  ArrayList<String>();
+        zone5.add("Stockholm");
+        zone5.add("Berlin");
+        zone5.add("Helsinki");
+        zone5.add("Rome");
+        zone5.add("Quiet Room 5-04");
+        zone5.add("Quiet Room 5-10");
+
+        List<String> zone6 =  new  ArrayList<String>();
+        zone6.add("Wellington");
+        zone6.add("Canberra");
+        zone6.add("Tokyo");
+        zone6.add("Warsaw");
+        zone6.add("Kiev");
+        zone6.add("Moscow");
+        zone6.add("Quiet Room 6-01");
+        zone6.add("Quiet Room 6-15");
+        zone6.add("Quiet Room 6-16");
+        zone6.add("Quiet Room 6-29");
+        zone6.add("Quiet Room 6-30");
+        zone6.add("Quiet Room 6-36");
+
+
+        List<String> zone7 =  new ArrayList<String>();
+        zone7.add("Cape Town");
+        zone7.add("Jakarta");
+        zone7.add("The Spectacular N");
+        zone7.add("The Canyons");
+        zone7.add("Quiet Room 7-08");
+        zone7.add("Quiet Room 7-09");
+        zone7.add("Quiet Room 7-11");
+        zone7.add("Quiet Room 7-14");
+        zone7.add("Quiet Room 7-19");
+        zone7.add("Quiet Room 7-20");
+
+        List<String> zone8 = new  ArrayList<String>();
+        zone8.add("Honolulu");
+        zone8.add("Ottowa");
+        zone8.add("Anchorage");
+        zone8.add("Quiet Room 8-12");
+        zone8.add("Quiet Room 8-18");
+        zone8.add("Quiet Room 8-23");
+        zone8.add("Quiet Room 8-28");
+        zone8.add("Quiet Room 8-29");
+        zone8.add("Quiet Room 8-30");
+        zone8.add("Quiet Room 8-34");
+        zone8.add("Quiet Room 8-35");
+
+        List<String> zone9 = new  ArrayList<String>();
+        zone9.add("Buenos Aires");
+        zone9.add("Brasilla");
+        zone9.add("Washington DC");
+        zone9.add("V.A Lab");
+        zone9.add("Quiet Room 9-06");
+        zone9.add("Quiet Room 9-17");
+        zone9.add("Quiet Room 9-25");
+        zone9.add("Quiet Room 9-27");
+        zone9.add("Quiet Room 9-39");
+        zone9.add("Quiet Room 9-41");
+        zone9.add("Quiet Room 9-48");
+
+
+        listDataChild.put(listDataHeader.get(0), zone1); // Header,
+        listDataChild.put(listDataHeader.get(1), zone3);
+        listDataChild.put(listDataHeader.get(2), zone4);
+        listDataChild.put(listDataHeader.get(3), zone5);
+        listDataChild.put(listDataHeader.get(4), zone6);
+        listDataChild.put(listDataHeader.get(5), zone7);
+        listDataChild.put(listDataHeader.get(6), zone8);
+        listDataChild.put(listDataHeader.get(7), zone9);
+
+    }
+}
