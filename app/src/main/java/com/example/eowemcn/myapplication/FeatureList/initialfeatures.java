643b571de0517a1784d016131c0073ec88e0671f
Darragh Burke <darragh.b.burke@ericsson.com>
2017-06-16 11:31:11 +0100

    Added new activity + layout for new features screen

---
+++ app/src/main/java/com/example/eowemcn/myapplication/FeatureList/initialfeatures.java
@@ -0,0 +1,16 @@
+package com.example.eowemcn.myapplication.FeatureList;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.eowemcn.myapplication.R;
+
+public class initialfeatures extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.features);
+    }
+}
--- app/src/main/java/com/example/eowemcn/myapplication/initialui.java
+++ app/src/main/java/com/example/eowemcn/myapplication/initialui.java
@@ -9,42 +9,60 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.eowemcn.myapplication.FeatureList.initialfeatures;
 import com.example.eowemcn.myapplication.list.ListActivity;
 import com.example.eowemcn.myapplication.map.Maps;
 
 public class initialui extends Activity {
 
     public Button textview2;
-        public void init2() {
-            textview2 = (Button) findViewById(R.id.textview2);
-            textview2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent newscreen1 = new Intent(initialui.this, Maps.class);
 
-                    startActivity(newscreen1);
-                }
-            });
-        }
+    public void init2() {
+        textview2 = (Button) findViewById(R.id.textview2);
+        textview2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newscreen2 = new Intent(initialui.this, Maps.class);
+
+                startActivity(newscreen2);
+            }
+        });
+    }
 
     public Button textview1;
 
-        public void init(){
-            textview1 = (Button)findViewById(R.id.textview1);
-            textview1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+    public void init() {
+        textview1 = (Button) findViewById(R.id.textview1);
+        textview1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                    Intent newscreen = new Intent(initialui.this,ListActivity.class);
+                Intent newscreen = new Intent(initialui.this, ListActivity.class);
 
-                    startActivity(newscreen);
+                startActivity(newscreen);
 
-                }
-            });
+            }
+        });
+    }
+
+    public Button textview3;
+
+    public void init3() {
+        textview3 = (Button) findViewById(R.id.textview3);
+        textview3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent newscreen3 = new Intent(initialui.this, initialfeatures.class);
+
+                startActivity(newscreen3);
+            }
+        });
+    }
 
 
 
-                }
+
 
     @Override
     protected void onCreate(Bundle savedInstancesState) {
@@ -65,6 +83,7 @@ public class initialui extends Activity {
 
         init();
         init2();
+        init3();
 
     }
 
